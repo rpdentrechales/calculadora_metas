112f52d9c6545fce259ebb15a8efcacae5d80105
cironis <ciroshia@gmail.com>
2025-04-02 12:39:19 -0300

    update

--- views/calculadora.py
+++ views/calculadora.py
@@ -6,16 +6,18 @@ st.set_page_config(page_title="Calculadora de Metas", page_icon="💎",layout="w
 
 st.title("Calculadora de Metas")
 
-col_1,col_2,spacer = st.columns([1,1,5],gap="small",vertical_alignment="center")
+grid_1,grid_2,spacer = st.columns([1,1,5],gap="small",vertical_alignment="center")
 
 if 'meta_rede' in st.session_state:
     
     meta_rede = st.session_state['meta_rede']
 
 else:
-   meta_rede = 0
+   meta_rede = None
 
    
-meta_rede_display = col_1.metric(label="Meta Rede",value=meta_rede)
-rede_popover= col_2.popover("Ajustar")
+meta_rede_display = grid_1.metric(label="Meta Rede",value=meta_rede)
+rede_popover= grid_2.popover("Ajustar")
+popoover_col_1, popoover_col_2 = rede_popover.columns(2)
 meta_rede = rede_popover.number_input(label="Meta Rede (R$)",min_value=0.0)
+meta_rede_nome = popoover_col_1.toggle(label="deixar em branco",value=False)
