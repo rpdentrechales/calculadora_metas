a2f9a5e7a6788485384bfd698ee8404757acab0a
cironis <ciroshia@gmail.com>
2025-04-02 12:04:39 -0300

    update

--- views/calculadora.py
+++ views/calculadora.py
@@ -6,10 +6,10 @@ st.set_page_config(page_title="Calculadora de Metas", page_icon="💎",layout="w
 
 st.title("Calculadora de Metas")
 
-col_1,col_2,spacer = st.columns([1,1,10])
+col_1,col_2,spacer = st.columns([1,1,5])
 
 with col_1:
     
-  meta_rede = st.number_input(label="Meta Rede")
+  meta_rede = st.number_input(label="Meta Rede",format="R$%0.2f")
 
 st.write(meta_rede)
