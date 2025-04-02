d714e21d7734fd8a733037125c97371ecc8b4845
cironis <ciroshia@gmail.com>
2025-04-02 12:41:06 -0300

    update

--- views/calculadora.py
+++ views/calculadora.py
@@ -18,6 +18,8 @@ else:
    
 meta_rede_display = grid_1.metric(label="Meta Rede",value=meta_rede)
 rede_popover= grid_2.popover("Ajustar")
-popoover_col_1, popoover_col_2 = rede_popover.columns(2)
+
 meta_rede = rede_popover.number_input(label="Meta Rede (R$)",min_value=0.0)
+popoover_col_1, popoover_col_2 = rede_popover.columns(2)
 meta_rede_nome = popoover_col_1.toggle(label="deixar em branco",value=False)
+meta_rede_salvar = popoover_col_2.button(label="Salvar")
