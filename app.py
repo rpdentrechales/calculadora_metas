e6d0a327d1713d21a1f2a6155db864c519cfa495
cironis <ciroshia@gmail.com>
2025-04-02 11:45:11 -0300

    update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # --- PAGE SETUP ---
-sample_page = st.Page(
-    "views/page_1.py",
-    title="Sample Page 1",
-    icon=":material/thumb_up:",
+calculadora_page = st.Page(
+    "views/calculadora.py",
+    title="Calculadora de metas",
+    icon=":material/calculate:",
     default=True,
 )
 
@@ -14,7 +14,7 @@ sample_page = st.Page(
 # --- NAVIGATION SETUP [WITH SECTIONS]---
 pg = st.navigation(
     {
-        "Pages": [sample_page]
+        "Pages": [calculadora_page]
     }
 )
 
---
+++ views/calculadora.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import datetime
+
+st.set_page_config(page_title="Calculadora de Metas", page_icon="💎",layout="wide")
+
+st.title("Calculadora de Metas")
+
+porcentagem = st.slider(label="%", min_value=0, max_value=100, step=1,label_visibility=False)
+st.write(porcentagem)
