b379fe71b9c66693ed44e90570f6ac45f5e7d048
cironis <ciroshia@gmail.com>
2025-04-02 13:43:54 -0300

    update

--- auxiliar/aux_display.py
+++ auxiliar/aux_display.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-def test():
-    st.write("teste")
+def atualizar_valores(update_var,update_arg,update_value):
+    st.session_state[update_var][update_arg] = st.session_state[update_value]
--- views/calculadora.py
+++ views/calculadora.py
@@ -14,11 +14,26 @@ if 'meta_rede' in st.session_state:
     meta_rede = st.session_state['meta_rede']
 
 else:
-   meta_rede = None
+   meta_rede = {"value":None,
+                "dont_use":False}
 
    
 meta_rede_display = grid_1.metric(label="Meta Rede",value=meta_rede)
 rede_popover= grid_2.popover("Ajustar")
 
-meta_rede = rede_popover.number_input(label="Meta Rede (R$)",min_value=0.0,value=None,on_change=test)
-meta_rede_nome = rede_popover.toggle(label="deixar em branco",value=False,on_change=test)
+meta_rede = rede_popover.number_input(label="Meta Rede (R$)",
+                                      min_value=0.0,
+                                      value=None,
+                                      on_change=atualizar_valores,
+                                      key="meta_rede_value",
+                                      kwargs = {"update_var":"meta_rede",
+                                                "update_arg":"value",
+                                                "update_value":"meta_rede_value"})
+
+meta_rede_nome = rede_popover.toggle(label="deixar em branco",
+                                     value=False,
+                                     on_change=atualizar_valores,
+                                     key="meta_rede_nome",
+                                     kwargs = {"update_var":"meta_rede",
+                                                "update_arg":"dont_use",
+                                                "update_value":"meta_rede_nome"})
