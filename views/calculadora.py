8d952fe6a6dd5a8f2642e539edb87ca9d0e7ec68
cironis <ciroshia@gmail.com>
2025-04-03 12:48:48 -0300

    update

--- views/calculadora.py
+++ views/calculadora.py
@@ -7,10 +7,16 @@ st.set_page_config(page_title="Calculadora de Metas", page_icon="💎",layout="w
 
 st.title("Calculadora de Metas")
 
-grid_1,grid_2,spacer = st.columns([1,1,5],gap="small",vertical_alignment="center")
+grid = st.columns(6,gap="small",vertical_alignment="center")
 
-lista_de_variaveis = ["meta_rede"]
+lista_de_variaveis = ["total_rede","total_fb","total_google"]
 iniciar_variaveis(lista_de_variaveis)
    
-meta_rede_display = imprimir_metric("meta_rede","Meta Rede",grid_1)
-rede_popover = imprimir_popover("Meta Rede (R$)","meta_rede", grid_2)
+imprimir_metric("total_rede","Total Rede",grid[0])
+imprimir_popover("Total Rede (R$)","total_rede", grid[1])
+
+imprimir_metric("total_google","Total Google",grid[0])
+imprimir_popover("Total Google (R$)","total_google", grid[1])
+
+imprimir_metric("total_fb","Total Facebook",grid[0])
+imprimir_popover("Total Facebook (R$)","total_fb", grid[1])
